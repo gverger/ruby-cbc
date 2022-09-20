9e1883085748b1d80320586f1b30db26b601e95c
Guillaume Verger <verger.guillaume@gmail.com>
2022-09-21 00:54:27 +0200

    Add tests to problem_unwrap

--- lib/ruby-cbc/utils/problem_unwrap.rb
+++ lib/ruby-cbc/utils/problem_unwrap.rb
@@ -18,34 +18,30 @@ module Cbc
         m
       end
 
-      def variables(model)
-        nb_vars = Cbc_wrapper.Cbc_getNumCols(cbc_model)
-        lower_bounds = Cbc_wrapper::DoubleArray.frompointer(Cbc_wrapper.Cbc_getColLower(cbc_model))
-        upper_bounds = Cbc_wrapper::DoubleArray.frompointer(Cbc_wrapper.Cbc_getColUpper(cbc_model))
-        nb_vars.times do |i|
-          Cbc_wrapper.Cbc_getColName(cbc_model, i, name_container, max_name_size)
-          name = CString.from_c(name_container)
+      private
 
+      def variables(model)
+        lower_bounds = double_array(Cbc_wrapper.Cbc_getColLower(cbc_model))
+        upper_bounds = double_array(Cbc_wrapper.Cbc_getColUpper(cbc_model))
+
+        nb_vars.times do |i|
           range = float_value(lower_bounds[i])..float_value(upper_bounds[i])
           if Cbc_wrapper.Cbc_isInteger(cbc_model, i) == 1
-            model.int_var(range, name: name)
+            model.int_var(range, name: var_name(i))
           else
-            model.cont_var(range, name: name)
+            model.cont_var(range, name: var_name(i))
           end
         end
-        model.to_s
       end
 
       def constraints(model)
-        nb_vars = Cbc_wrapper.Cbc_getNumCols(cbc_model)
-        nb_cons = Cbc_wrapper.Cbc_getNumRows(cbc_model)
-        lower_bounds = Cbc_wrapper::DoubleArray.frompointer(Cbc_wrapper.Cbc_getRowLower(cbc_model))
-        upper_bounds = Cbc_wrapper::DoubleArray.frompointer(Cbc_wrapper.Cbc_getRowUpper(cbc_model))
+        lower_bounds = double_array(Cbc_wrapper.Cbc_getRowLower(cbc_model))
+        upper_bounds = double_array(Cbc_wrapper.Cbc_getRowUpper(cbc_model))
 
         nb_elements = Cbc_wrapper.Cbc_getNumElements(cbc_model)
-        starts = Cbc_wrapper::IntArray.frompointer(Cbc_wrapper.Cbc_getVectorStarts(cbc_model))
-        indices = Cbc_wrapper::IntArray.frompointer(Cbc_wrapper.Cbc_getIndices(cbc_model))
-        elements = Cbc_wrapper::DoubleArray.frompointer(Cbc_wrapper.Cbc_getElements(cbc_model))
+        starts = int_array(Cbc_wrapper.Cbc_getVectorStarts(cbc_model))
+        indices = int_array(Cbc_wrapper.Cbc_getIndices(cbc_model))
+        elements = double_array(Cbc_wrapper.Cbc_getElements(cbc_model))
 
         cons_terms = Array.new(nb_cons) { [] }
 
@@ -54,12 +50,11 @@ module Cbc
           from = starts[var_idx]
           to = var_idx == nb_vars - 1 ? nb_elements : starts[var_idx + 1]
 
-          puts "new values for #{var_idx}"
+          var = model.vars[var_idx]
           (from...to).each do |i|
             cons_idx = indices[i]
-            element = elements[i]
-            puts "#{cons_idx}  : #{element}"
-            cons_terms[cons_idx] << model.vars[var_idx] * element
+            mult = elements[i]
+            cons_terms[cons_idx] << var * mult
           end
 
           var_idx += 1
@@ -69,10 +64,8 @@ module Cbc
           low = float_value(lower_bounds[cons_idx])
           up = float_value(upper_bounds[cons_idx])
 
-          Cbc_wrapper.Cbc_getRowName(cbc_model, cons_idx, name_container, max_name_size)
-          name = CString.from_c(name_container)
+          name = cons_name(cons_idx)
           terms = Ilp::TermArray.new(terms)
-          puts terms
 
           if low == up
             model.enforce(name => terms == low)
@@ -81,8 +74,6 @@ module Cbc
             model.enforce(name => terms <= up) if up != Cbc::INF
           end
         end
-
-        model.to_s
       end
 
       OBJ_IGNORE = 0
@@ -93,8 +84,7 @@ module Cbc
         obj_sense = Cbc_wrapper.Cbc_getObjSense(cbc_model)
         return if obj_sense == OBJ_IGNORE
 
-        nb_vars = Cbc_wrapper.Cbc_getNumCols(cbc_model)
-        coeffs = Cbc_wrapper::DoubleArray.frompointer(Cbc_wrapper.Cbc_getObjCoefficients(cbc_model))
+        coeffs = double_array(Cbc_wrapper.Cbc_getObjCoefficients(cbc_model))
         terms = (0...nb_vars).map { |i| model.vars[i] * coeffs[i] }
 
         if obj_sense == OBJ_MIN
@@ -110,6 +100,31 @@ module Cbc
         CString.from_c(name)
       end
 
+      def float_value(val)
+        return Cbc::INF if val == Float::MAX
+        return -Cbc::INF if val == -Float::MAX
+
+        val
+      end
+
+      def nb_vars
+        Cbc_wrapper.Cbc_getNumCols(cbc_model)
+      end
+
+      def nb_cons
+        Cbc_wrapper.Cbc_getNumRows(cbc_model)
+      end
+
+      def var_name(var_idx)
+        Cbc_wrapper.Cbc_getColName(cbc_model, var_idx, name_container, max_name_size)
+        CString.from_c(name_container)
+      end
+
+      def cons_name(cons_idx)
+        Cbc_wrapper.Cbc_getRowName(cbc_model, cons_idx, name_container, max_name_size)
+        CString.from_c(name_container)
+      end
+
       def name_container
         @name_container ||= " " * max_name_size
       end
@@ -119,11 +134,12 @@ module Cbc
         @max_name_size ||= Cbc_wrapper.Cbc_maxNameLength(cbc_model) + 1
       end
 
-      def float_value(v)
-        return Cbc::INF if v == Float::MAX
-        return -Cbc::INF if v == -Float::MAX
+      def int_array(ptr)
+        Cbc_wrapper::IntArray.frompointer(ptr)
+      end
 
-        v
+      def double_array(ptr)
+        Cbc_wrapper::DoubleArray.frompointer(ptr)
       end
     end
   end
--- spec/utils/problem_unwrap_spec.rb
+++ spec/utils/problem_unwrap_spec.rb
@@ -13,12 +13,52 @@ module Cbc
 
       it "returns the right name" do
         p = problem_unwrap
-        expect(p.problem_name).to eq "EXAMPLE"
+        expect(p.to_model.name).to eq "EXAMPLE"
       end
 
-      it "gets the max name size" do
+      it "gets the variables right" do
         p = problem_unwrap
-        expect(p.max_name_size).to eq 6
+        vars = p.to_model.vars
+        expect(vars.size).to eq 8
+
+        col01 = vars.find { |var| var.name == "COL01" }
+        expect(col01).to_not be_nil
+        expect(col01).to have_attributes(lower_bound: 2.5, upper_bound: Cbc::INF, kind: :continuous)
+
+        col02 = vars.find { |var| var.name == "COL02" }
+        expect(col02).to_not be_nil
+        expect(col02).to have_attributes(lower_bound: 0, upper_bound: 4.1, kind: :continuous)
+
+        col03 = vars.find { |var| var.name == "COL03" }
+        expect(col03).to_not be_nil
+        expect(col03).to have_attributes(lower_bound: 0, upper_bound: 1.0, kind: :integer)
+
+        col05 = vars.find { |var| var.name == "COL05" }
+        expect(col05).to_not be_nil
+        expect(col05).to have_attributes(lower_bound: 0.5, upper_bound: 4.0, kind: :continuous)
+      end
+
+      it "gets the constraints right" do
+        p = problem_unwrap
+        constraints = p.to_model.constraints
+        expect(constraints.size).to eq 7
+
+        row01 = constraints.find { |cons| cons.function_name == "ROW01" }
+        expect(row01).to_not be_nil
+        expect(row01.to_s)
+          .to eq "+ 3.0 COL01 + COL02 - 2.0 COL04 - 1.0 COL05 - 1.0 COL08 >= 2.5"
+
+        row02 = constraints.find { |cons| cons.function_name == "ROW02" }
+        expect(row02).to_not be_nil
+        expect(row02.to_s)
+          .to eq "+ 2.0 COL02 + 1.1 COL03 <= 2.1"
+      end
+
+      it "gets the objective right" do
+        p = problem_unwrap
+        obj = p.to_model.objective
+
+        expect(obj.to_s).to eq "Minimize\n  + COL01 + 2.0 COL05 - 1.0 COL08"
       end
     end
   end
